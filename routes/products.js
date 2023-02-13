6ea3f7e0bf5d538c49cd2b25375de7dc78864574
Bella Ranalli <bela.ranalli@gmail.com>
2023-02-13 19:32:00 -0300

    completados las rutas de product

--- routes/products.js
+++ routes/products.js
@@ -14,7 +14,7 @@ productRouter.post('/', async (req, res) => {
     }
 });
 
-productRouter.get('/', async(req, res) => {
+productRouter.get('/', async (req, res) => {
     try {
         let products = await productManager.getProducts();
         res.send(products);
@@ -23,5 +23,39 @@ productRouter.get('/', async(req, res) => {
         res.send('Errror')
     }
 });
+
+productRouter.get('/:pid', async (req, res) => {
+    let pid = parseInt(req.params.pid)
+    let saveId = await productManager.getProductById(pid);
+    if (saveId){
+        res.send(saveId)
+    }else{
+        res.send('Producto no encontrado')
+    }
+})
+
+productRouter.put('/:pid', async (req, res) =>{
+    let pid = parseInt(req.params.pid)
+    let{campo, actualizacion} = req.body
+
+    
+    
+    try {
+        let newProduct = productManager.updateProductById(pid, campo, actualizacion);
+        res.send("Producto Actualizado")
+    } catch (error) {
+        res.send("Producto no encontrado")
+    }
+})
+
+productRouter.delete('/:pid', async (req, res) =>{
+    let pid = parseInt(req.params.id)
+    try {
+        let deleteById = productManager.deleteProduct(pid)
+        res.send("Producto eliminado")
+    } catch (error) {
+        res.send('Producto no encontrado')
+    }
+})
 
 export default productRouter;
