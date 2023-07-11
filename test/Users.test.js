7149a5a357fa5875802f6113f3801d2a4222b092
Bella Ranalli <bela.ranalli@gmail.com>
2023-07-11 18:47:10 -0300

    test mocha

--- test/Users.test.js
+++ test/Users.test.js
@@ -2,19 +2,17 @@ import mongoose from "mongoose";
 import Users from "../dao/usersDao.js";
 import Assert from 'assert';
 
-const URL = process.env.NODE_URI;
-
-mongoose.connect(URL);
+mongoose.connect('mongodb+srv://BellaDev:aNabella1702@cluster0.rvjajgv.mongodb.net/ecommerce?retryWrites=true&w=majority')
 const assert = Assert.strict;
 
 describe('Testing Users Dao', () => {
-    before(function () {
-        this.usersDao = new Users();
+    before( async function () {    
+        this.usersDao =  Users;
     });
 
     it('El Dao debe poder obtener los usuarios en forma de arreglo', async function () {
         console.log(this.usersDao);
-        const result = await this.usersDao.get();
+        const result = await this.usersDao.getUsers();
         assert.strictEqual(Array.isArray(result), true);
     });
 
